bd6fab8e22d732107af8fa3b9f8d13616299e34b
AlejandroFerroBejerano <alejandroferro.1988@gmail.com>
2019-04-22 11:02:38 +0200

    Added brief_desc and description variables

--- product_builder.py
+++ product_builder.py
@@ -1,7 +1,7 @@
 import codecs
 sfilename = "txt_3_primeros.txt"
 dfilename = "result.txt"
-f = open(filename, "r", encoding="latin-1")
+f = open(sfilename, "r", encoding="latin-1")
 data = f.read()
 data = data.replace("Avda. de las canteras 23-25 Nave 5, Políg. Industrial Valmor.","")
 data = data.replace("Avda. de las canteras 23-25 Nave 5, Políg. Industrial Valmor","")
@@ -11,8 +11,13 @@ data = data.replace("E-mail: tecnico@kleer-kim.com Web site: kleer-kim.com","NEW
 data = data.replace("Tel. 91 895 65 10 Fax. 91 895 66 08","")
 products=data.split("NEWPRODUCT")
 len(products)
-result = open(dfilename,"w",encoding="latin-1")
-for product in products:
-  prod_desc = product[1].split("DESCRIPCION\n")[1].split("APLICACIONES\n")[0]
-  prod_desc = prod_desc.replace("\n","")
-  result.write(prod_desc)
+result = open(dfilename,"w+",encoding="latin-1")
+for x in range(len(products)-1):
+  #descripcion
+  prod_desc = products[x].split("DESCRIPCION\n")[1].split("APLICACIONES\n")[0]
+  prod_desc = prod_desc.replace("\n"," ")
+  #descripcion corta
+  brief_desc = prod_desc.split(".")[0]
+  result.write(brief_desc + " | " + prod_desc+ "\n")
+
+result.close()
